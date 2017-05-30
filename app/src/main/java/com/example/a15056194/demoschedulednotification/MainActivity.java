515b873a605df11da60c0e26dece2c438ca6dac8
Joelljw <15056194@myrp.edu.sg>
2017-05-30 12:14:33 +0800

    Done Schedule notification

--- app/src/main/java/com/example/a15056194/demoschedulednotification/MainActivity.java
+++ app/src/main/java/com/example/a15056194/demoschedulednotification/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.a15056194.demoschedulednotification;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +26,16 @@ public class MainActivity extends AppCompatActivity {
         btnSchedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
 
+                Intent intent = new Intent(MainActivity.this, ScheduledNotificationReceiver.class);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, reqCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
             }
         });
     }
